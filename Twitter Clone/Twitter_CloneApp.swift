fb06779171db7fe9b9760a7872599d6bef112c67
YatharthDixit <yatharthdixit@icloud.com>
2022-11-20 21:35:49 +0530

    Initial Commit

---
+++ Twitter Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter Clone
+//
+//  Created by Yatharth Dixit on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
