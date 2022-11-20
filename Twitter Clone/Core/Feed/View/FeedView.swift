39bd86cdbdcdbb8a129c4f4c06bda081944c2f24
YatharthDixit <yatharthdixit@icloud.com>
2022-11-20 21:56:58 +0530

    Initial Commit

---
+++ Twitter Clone/Core/Feed/View/FeedView.swift
@@ -0,0 +1,26 @@
+//
+//  FeedView.swift
+//  Twitter Clone
+//
+//  Created by Yatharth Dixit on 20/11/22.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack{
+                ForEach(0 ... 20, id: \.self){
+                    _ in Text("Tweet Row View")
+                }
+            }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
